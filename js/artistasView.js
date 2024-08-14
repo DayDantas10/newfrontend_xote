8b20716a1495c8ae622d7af078fc651af6e8c654
git\Dayane <dayane.lima@escolar.ifrn.edu.br>
2024-08-14 19:53:13 -0300

    Atualizações da view

--- js/artistasView.js
+++ js/artistasView.js
@@ -1,15 +1,31 @@
 export function apresentarArtistas(artistas){
 
-    console.log("ARTISTAS ################");
-    console.log(artistas);
-
-    const linhas = document.querySelector(".row");
+    let div = document.querySelector(".row");
+    let componente = "";
 
     artistas.forEach(artista => {
-        const div = document.createElement("div");
-        div.class = "col-md-6 col-lg-4";
-        
-        
+        componente = componente  + `
+        <div class="col-md-6 col-lg-4">
+          <div class="box"> <!-- appendChild-->
+            <div class="img-box">
+              <img src="images/${artista.imagem}" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                ${artista.nome_artista}
+              </h5>
+              <div class="price_box">
+                <h6 class="price_heading">
+                  <span>$</span> ${artista.cidade}
+                </h6>
+                <a href="">
+                  ${artista.estado}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>`;                
     });
+    div.innerHTML = componente;
     
 }
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 // to get current year
+/*
 function getYear() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
@@ -35,10 +36,11 @@ $('.slick_slider_nav').slick({
 
 /** google_map js **/
 
+/*
 function myMap() {
     var mapProp = {
         center: new google.maps.LatLng(40.712775, -74.005973),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+}*/
