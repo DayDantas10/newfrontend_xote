32d3a4f73897576c168b8f27d08b0d8e854cda90
Dayane <dayane.lima@escolar.ifrn.edu.br>
2024-11-06 20:34:08 -0300

    mudanças 06/11

--- js/entrevistasView.js
+++ js/entrevistasView.js
@@ -1,34 +1,31 @@
 
-
-
 export function apresentarEntrevistas(entrevistas){
 
     let div = document.querySelector(".row");
     let componente = "";
 
-    entrevistas.forEach(entrevistas => {
+    entrevistas.forEach(entrevista => {
         componente = componente  + `
-        <div class="col-md-6 col-lg-4">
-          <div class="box"> <!-- appendChild-->
+        <div class="row">
+        <div class="col-md-6 col-lg-4 mx-auto">
+          <div class="box">
             <div class="img-box">
-              <img src="images/${artista.imagem}" alt="">
+            <img src="images/${entrevista.imagem}" alt="">
             </div>
             <div class="detail-box">
               <h5>
-                ${artista.nome_artista}
+              ${entrevista.nome}
               </h5>
-              <div class="price_box">
-                <h6 class="price_heading">
-                  ${artista.cidade}
-                </h6>
-                <a href="">
-                  ${artista.estado}
-                </a>
-              </div>
+              <p>
+              ${entrevista.dados_entrevista}
+              </p>
+              <a href="">
+                Read  ${artista.estado} More
+              </a>
             </div>
           </div>
-        </div>`;                
+        </div>
+      </div>`;                
     });
-    div.innerHTML = componente;
-    
+    div.innerHTML = componente;  
 }
